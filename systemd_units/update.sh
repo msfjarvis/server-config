d996c707c62beaf5eb2de290a99832292c9bf171
Harsh Shandilya <me@msfjarvis.dev>
2020-12-31 21:40:46 +0530

    systemd: revive walls-bot-rs
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- systemd_units/update.sh
+++ systemd_units/update.sh
@@ -8,7 +8,7 @@ function prettyPrint() {
 }
 
 # Grab all service names
-declare -a services=('mirror-bot' 'mirror-bot-2' 'uno-bot')
+declare -a services=('mirror-bot' 'mirror-bot-2' 'walls-bot-rs')
 
 # Now loop through each service and install it
 for service in "${services[@]}"; do
