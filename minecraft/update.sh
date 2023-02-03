9011d1f82a15448ab0157ecec87296fa515efd59
Harsh Shandilya <me@msfjarvis.dev>
2023-02-03 20:42:51 +0530

    minecraft: factor out game version

--- minecraft/update.sh
+++ minecraft/update.sh
@@ -2,16 +2,18 @@
 
 set -euo pipefail
 
+MINECRAFT_VERSION="1.19.3"
+
 function update_paper() {
   local CURRENT_VERSION LATEST_VERSION
   CURRENT_VERSION="$(cat .paper-version)"
-  LATEST_VERSION="$(curl -s 'https://papermc.io/api/v2/projects/paper/versions/1.19.3' -H 'accept: application/json' | jq .builds[-1])"
+  LATEST_VERSION="$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${MINECRAFT_VERSION}" -H 'accept: application/json' | jq .builds[-1])"
   echo "Currently running version is ${CURRENT_VERSION}"
-  echo "Latest version is 1.19.3-${LATEST_VERSION}"
-  [ -f "paper-1.19.3-${LATEST_VERSION}.jar" ] && return
-  echo "Updating to 1.19.3-${LATEST_VERSION}"
-  curl --progress-bar -L "https://papermc.io/api/v2/projects/paper/versions/1.19.3/builds/${LATEST_VERSION}/downloads/paper-1.19.3-${LATEST_VERSION}.jar" -o "paper-1.19.3-${LATEST_VERSION}.jar"
-  echo "1.19.3-${LATEST_VERSION}" >.paper-version
+  echo "Latest version is ${MINECRAFT_VERSION}-${LATEST_VERSION}"
+  [ -f "paper-${MINECRAFT_VERSION}-${LATEST_VERSION}.jar" ] && return
+  echo "Updating to ${MINECRAFT_VERSION}-${LATEST_VERSION}"
+  curl --progress-bar -L "https://papermc.io/api/v2/projects/paper/versions/${MINECRAFT_VERSION}/builds/${LATEST_VERSION}/downloads/paper-${MINECRAFT_VERSION}-${LATEST_VERSION}.jar" -o "paper-${MINECRAFT_VERSION}-${LATEST_VERSION}.jar"
+  echo "${MINECRAFT_VERSION}-${LATEST_VERSION}" >.paper-version
 }
 
 function cleanup_previous_jars() {
